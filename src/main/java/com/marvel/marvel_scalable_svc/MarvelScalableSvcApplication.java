7fa6f4532660f3fe1e7709cae47d9f0de26252d1
jsiddiqui <jsiddiqui@wsgc.com>
2024-07-19 16:19:45 +0500

    Marvel Java Scalable Service

---
+++ src/main/java/com/marvel/marvel_scalable_svc/MarvelScalableSvcApplication.java
@@ -0,0 +1,13 @@
+package com.marvel.marvel_scalable_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelScalableSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelScalableSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marvel/marvel_scalable_svc/MarvelScalableSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvel.marvel_scalable_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelScalableSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
